9e798c5140fc907d38ab652feac0e822b2389939
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-03-08 19:50:56 -0700

    loop, endQuiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,8 @@ var startButton = document.querySelector('#start-button');
 var questionAnswerEl = document.querySelector("#question-answers");
 var startScreen = document.querySelector("#start-screen");
 var timerId = document.querySelector('#timer-id');
+var endScreen = document.querySelector('#end-screen');
+
 
 
 
@@ -31,11 +33,13 @@ function triviaRules() {
 // Timer
 
 var timeSeconds = 30
-
+var countInterval
 timer.innerHTML = timeSeconds;
 
 function countDown() {
-    var countInterval = setInterval(function() {
+    clearInterval(countInterval)
+    timeSeconds = 30
+    countInterval = setInterval(function() {
         if (timeSeconds === 0) {
             clearInterval(countInterval);
             timer.innerHTML = "Time Out!";
@@ -84,90 +88,115 @@ async function getRandomUser() {
     });
 }
 
-async function getQuestions() {
-    var response = await fetch("https://opentdb.com/api.php?amount=5");
-    var data = await response.json();
-    return data
-}
-
-function randomizeAnswers(arr) {
-    for (var i = arr.length - 1; i >= 0; i--) {
-        var s = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[s]] = [arr[s], arr[i]];
-    }
-}
-
-getQuestions().then((data) => {
-    var results = data.results[0];
-    console.log(results);
-    document.getElementById('question').innerHTML = results.question;
-    var answers = [...results.incorrect_answers, results.correct_answer];
-    randomizeAnswers(answers);
-    for (var i = 0; i < 4; i++) {
-        var index = i + 1;
-        document.getElementById(`choice${index}label`).innerHTML = answers[i];
-        document.getElementById(`choice${index}`).value = answers[i];
-    }
-
-    document.getElementById('guess').addEventListener('click', () => {
-        document.querySelectorAll('input[name="choice"]').forEach((el) => {
-            var accuracy = document.getElementById('accuracy');
-            if(el.checked){
-                console.log(el.value);
-                console.log(results.correct_answer);
-
-                if(el.value === results.correct_answer) {
-                    accuracy.innerHTML = "Good job!"
-                } else accuracy.innerHTML = `Sorry! The correct answer is ${results.correct_answer}`;
-            } 
-        });
-    });
-});
-
-
-// //open trivia api fetch
-// async function sendRequest() {
-//     const apiUrl = 'https://opentdb.com/api.php?amount=5';
-//     const result = await fetch(apiUrl);
-//     const data = await result.json();
-//     console.log(data.results);
-//     useApiResponse(data.results[0]);
-// }
-// //load open trivia api data
-// sendRequest();
-
-// //load questions and and answers and make them random 
-// function useApiResponse(data) {
-//     var correctAnswer = data.correct_answer;
-//     var incorrectAnswer = data.incorrect_answers;
-//     var incorrectList = incorrectAnswer;
-//     incorrectList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
-//     // console.log(incorrectList);
-//     // console.log(correctAnswer);
-
-//     question.innerHTML = `
-//         ${data.question} <br> 
-//         <span class = "category">Catagory: ${data.category} </span> <br> 
-//         <span class = "difficulty">Difficulty: ${data.difficulty} </span>`;
-//     answers.innerHTML = `
-//         ${incorrectList.map((option, index) => `
-//             <button> ${index + 1}. <span>${option}</span> </button>
-//     `).join('')}
-//     `;
-// //     answers.addEventListener("click", function() {
-// //         //console.log(incorrectList)
-// //         if (correctAnswer) {
-// //             console.log("Correct!")
-// //         } else {
-// //             console.log("Incorrect!")
-// //         }
-// //         });
-//     //nextQuestion();
-// // }
+// async function getQuestions() {
+//     var response = await fetch("https://opentdb.com/api.php?amount=5");
+//     var data = await response.json();
+//     return data
 // }
 
-// function nextQuestion() {
-//     for (var i = 0; i < data.length; i++) {
+// function randomizeAnswers(arr) {
+//     for (var i = arr.length - 1; i >= 0; i--) {
+//         var s = Math.floor(Math.random() * (i + 1));
+//         [arr[i], arr[s]] = [arr[s], arr[i]];
+//     }
+// }
+
+// getQuestions().then((data) => {
+//     var results = data.results[index];
+//     console.log(results);
+//     document.getElementById('question').innerHTML = results.question;
+//     var answers = [...results.incorrect_answers, results.correct_answer];
+//     randomizeAnswers(answers);
+//     for (var i = 0; i < 4; i++) {
+//         var index = i + 1;
+//         document.getElementById(`choice${index}label`).innerHTML = answers[i];
+//         document.getElementById(`choice${index}`).value = answers[i];
 //     }
 
-// }
+//     document.getElementById('guess').addEventListener('click', () => {
+//         document.querySelectorAll('input[name="choice"]').forEach((el) => {
+//             var accuracy = document.getElementById('accuracy');
+//             if(el.checked){
+//                 console.log(el.value);
+//                 console.log(results.correct_answer);
+
+//                 if(el.value === results.correct_answer) {
+//                     accuracy.innerHTML = "Good job!"
+//                 } else accuracy.innerHTML = `Sorry! The correct answer is ${results.correct_answer}`;
+//             } 
+//         });
+//     });
+// });
+
+function renderQuestion(index) {
+
+}
+
+var index = 0
+var questions = []
+//open trivia api fetch
+async function sendRequest() {
+    const apiUrl = 'https://opentdb.com/api.php?amount=5';
+    const result = await fetch(apiUrl);
+    questions = await result.json();
+    console.log(questions.results);
+    renderQuestion(questions.results[index]);
+}
+//load open trivia api data
+sendRequest();
+
+//load questions and and answers and make them random 
+function renderQuestion(data) {
+    var correctAnswer = data.correct_answer;
+    var incorrectAnswer = data.incorrect_answers;
+    var incorrectList = incorrectAnswer;
+    incorrectList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+    // console.log(incorrectList);
+    // console.log(correctAnswer);
+
+    question.innerHTML = `
+        ${data.question} <br> 
+        <span class = "category">Catagory: ${data.category} </span> <br> 
+        <span class = "difficulty">Difficulty: ${data.difficulty} </span>`;
+    answers.innerHTML = `
+        ${incorrectList.map((option, index) => `
+            <button data-answer="${option}"> ${index + 1}. ${option} </button>
+    `).join('')}
+    `;
+    var clickCallback = function(event) {
+        //console.log(incorrectList)
+        var accuracy = event.target.dataset.answer
+        console.log(event.target)
+        if (correctAnswer === accuracy) {
+            console.log("Correct!")
+        } else {
+            console.log("Incorrect!")
+        }
+        answers.removeEventListener("click", clickCallback)
+        nextQuestion();
+        }
+    answers.addEventListener("click", clickCallback);
+}
+
+
+function nextQuestion() {
+    index ++
+    if (index < 5) {
+        renderQuestion(questions.results[index])
+        countDown()
+    } else {
+        endQuiz()
+    }
+
+}
+
+function endQuiz() {
+    console.log('Quiz has ended')
+
+    questionAnswerEl.setAttribute("class", "hide");
+
+    timerId.setAttribute("class", "hide");
+    
+    endScreen.setAttribute("class", "show")
+    
+}
